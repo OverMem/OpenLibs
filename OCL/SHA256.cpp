6e7ce08fe4181420b092073f6f757fa859fd5b8a
RemsPrgm <elecinfo.open@gmail.com>
2020-03-23 18:03:22 +0100

    Add GPL Licence & Deps sources

--- OCL/SHA256.cpp
+++ OCL/SHA256.cpp
@@ -1,3 +1,22 @@
+﻿/*
+Copyright © 2020 Nicolle Rémi
+
+This file is part of OpenLibs.
+
+OpenLibs is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenLibs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with OpenLibs.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "SHA256.h"
 
 SHA256::SHA256()
